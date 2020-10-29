e3a709c1724340477ad045423479751b686b6311
Filipe Mota <drete457@gmail.com>
2020-10-29 21:23:11 +0000

    build(element): loading screen

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import React, {useState} from 'react';
-import xlsxFile from 'read-excel-file';
-import './App.css';
+import React, {useState, useEffect, useCallback} from 'react';
+import exceltojson from './Components/exceltojson';
 
 export default function App() {
   const [excelFile, setExcelFile] = useState('');
   const [currentline, setLine] = useState({});
 
-  const handleUpload = (event) => {
-    const file = event.target.files[0];
-    //read excel file
-    xlsxFile(file).then((rows) => {
-      const rowFilter = rows.filter((row, index) => {
-        if (index > 3) {
-          return row;
-        }
-
-        return null;
-      })
-      
-      console.table(rowFilter)
-      setExcelFile(rowFilter)
-    });
-  };
-
   return (
     <>
     <input
-      type='file'
-      accept='.xlsx'
-      onChange={handleUpload}
+        type='file'
+        accept='.xlsx'
+        onChange={(event) => {
+          exceltojson(event).then(result => {
+            console.log(result)
+            setExcelFile(result)
+          });
+         
+          //setExcelFile(result);
+        }}
+
     />
-     
   </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Suspense fallback={'Loading...'}>
     <App />
-  </React.StrictMode>,
+  </Suspense>,
   document.getElementById('root')
 );
 
