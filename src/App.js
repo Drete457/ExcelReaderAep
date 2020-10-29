3cb6dc68957425047e73e8e61c50c4389d2fbfe8
Filipe Mota <drete457@gmail.com>
2020-10-29 22:31:08 +0000

    update(element): display correct loadingscreen

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import {
   CCardHeader,
   CCardBody,
   CInputFile,
-  CLabel
+  CLabel,
 } from '@coreui/react';
 import exceltojson from './Components/exceltojson';
 import DefaultLayout from './View/DefaultLayout';
+import LoadingScreen from './Components/loading';
 import './scss/style.scss';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [excelFile, setExcelFile] = useState('');
   const [currentline, setLine] = useState({});
 
@@ -26,22 +28,32 @@ export default function App() {
                 Conselho Jurisdicional - Visualizador do Controlo de Nomeações
               </CCardHeader>
               <CCardBody>
-              <CCol>
-                <CInputFile
-                  custom
-                  id="custom-file-input"
-                  accept=".xlsx"
-                  onChange={(event) => {
-                    exceltojson(event).then((result) => {
-                      console.log(result);
-                      setExcelFile(result);
-                    });
-                  }}
-                />
-                <CLabel htmlFor="custom-file-input" variant="custom-file">
-                    Pressiona aqui para escolheres o ficheiro
+                {isLoading && <LoadingScreen />}
+                {!excelFile && (
+                  <CCol>
+                    <CInputFile
+                      custom
+                      id="custom-file-input"
+                      accept=".xlsx"
+                      onChange={(event) => {
+                        setIsLoading(true);
+                        exceltojson(event).then((result) => {
+                          console.log(result);
+                          setExcelFile(result);
+                          setIsLoading(false);
+                        });
+                      }}
+                    />
+                    <CLabel htmlFor="custom-file-input" variant="custom-file">
+                      Pressiona aqui para escolheres o ficheiro
                     </CLabel>
-                    </CCol>
+                  </CCol>
+                )}
+                {excelFile.length > 0 && (
+                  <CCol>
+                    <p>teste</p>
+                  </CCol>
+                )}
               </CCardBody>
             </CCard>
           </CCol>
