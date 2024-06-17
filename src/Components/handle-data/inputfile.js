274a29e149a50d3c19f44b54bcffd0cb003a50b7
Filipe Mota <nada.geral@gmail.com>
2024-06-17 08:00:18 +0000

    fix a type on input file.js

--- src/Components/handle-data/inputfile.js
+++ src/Components/handle-data/inputfile.js
@@ -13,28 +13,26 @@ const InputFile = ({
   setExcelFile,
   setPositionOfEachInformation,
   setIsLoading,
-}) => {
-  return (
-    <CCol className="d-flex justify-content-between">
-      <CInputFile
-        custom
-        id="custom-file-input"
-        accept=".xlsx"
-        onChange={event => {
-          setIsLoading(true);
-          exceltojson(event, setPositionOfEachInformation).then(result => {
-            setExcelFile(result);
-            setTimeout(() => {
-              setIsLoading(false);
-            }, getRandomTime());
-          });
-        }}
-      />
-      <CLabel htmlFor="custom-file-input" variant="custom-file">
-        Pressiona aqui para escolheres o ficheiro
-      </CLabel>
-    </CCol>
-  );
-};
+}) => (
+  <CCol className="d-flex justify-content-between">
+    <CInputFile
+      custom
+      id="custom-file-input"
+      accept=".xlsx"
+      onChange={event => {
+        setIsLoading(true);
+        exceltojson(event, setPositionOfEachInformation).then(result => {
+          setExcelFile(result);
+          setTimeout(() => {
+            setIsLoading(false);
+          }, getRandomTime());
+        });
+      }}
+    />
+    <CLabel htmlFor="custom-file-input" variant="custom-file">
+      Pressiona aqui para escolheres o ficheiro
+    </CLabel>
+  </CCol>
+);
 
 export default InputFile;
