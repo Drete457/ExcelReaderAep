4f0c3120c733c4eb5508f6a9459a7dda8e0c7d5e
Filipe Mota <nada.geral@gmail.com>
2025-10-17 06:52:16 +0100

    refactor: simplify layout structure in DefaultLayout component and clean up styles

--- src/View/DefaultLayout/index.js
+++ src/View/DefaultLayout/index.js
@@ -3,10 +3,8 @@ import SuspenseFallback from '../../Components/feedback/SuspenseFallback';
 
 const DefaultLayout = ({ children }) => (
   <Suspense fallback={<SuspenseFallback message="A preparar o layout..." />}>
-    <div className="c-app c-default-layout">
-      <div className="c-wrapper">
-        <div className="c-body">{children}</div>
-      </div>
+    <div className="c-app">
+      {children}
     </div>
   </Suspense>
 );
